9a9985c73b7cc58d06cf13d1f1e8e706692c7161
lizavrmva <veremeevaaaa@gmail.com>
2022-08-30 20:59:52 +0200

    .

--- Assets/scrips/WanderingAI.cs
+++ Assets/scrips/WanderingAI.cs
@@ -8,9 +8,6 @@ public class WanderingAI : MonoBehaviour
     public float speed = 3.0f;
     public float obstacleRange = 5.0f;
 
-    [SerializeField] private GameObject fireballPrefab;
-    private GameObject _fireball;
-
     private bool _alive;
 
     void Start()
@@ -29,16 +26,7 @@ public class WanderingAI : MonoBehaviour
             if (Physics.SphereCast(ray, 0.75f, out hit))
             {
                 GameObject hitObject = hit.transform.gameObject;
-                if (hitObject.GetComponent<PlayerCharacter>())
-                {
-                    if (_fireball == null)
-                    {
-                        _fireball = Instantiate(fireballPrefab) as GameObject;
-                        _fireball.transform.position = transform.TransformPoint(Vector3.forward * 1.5f);
-                        _fireball.transform.rotation = transform.rotation;
-                    }
-                }
-                else if (hit.distance < obstacleRange)
+                if (hit.distance < obstacleRange)
                 {
                     float angle = Random.Range(-110, 110);
                     transform.Rotate(0, angle, 0);
